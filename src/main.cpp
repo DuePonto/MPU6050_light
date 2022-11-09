78667a62f13d7c097fdde4187276771cac47ceab
DuePonto <semirocketboy@gmail.com>
2022-11-10 00:19:52 +0300

    Init

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup() {
+  // put your setup code here, to run once:
+  mpu.begin();
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
